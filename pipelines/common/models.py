a0297ec3d484d590a7782b268a071bbedd932df4
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-11-13 12:04:10 +0000

    minor changes in small containers details

--- pipelines/common/models.py
+++
@@ -1,8 +0,0 @@
-
-
-class PipelineConfiguration():
-
-    def __init__(self, dockerHub, dockerHubContainer, dockerHubTags):
-        self.dockerHub = dockerHub
-        self.dockerHubContainer = dockerHubContainer
-        self.dockerHubTags = dockerHubTags
