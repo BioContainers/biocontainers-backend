7d54336211118b3fb11c703ad34f7e51291c3c1b
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-11-22 17:12:20 +0000

    path has been changed

--- biocontainers-flask/biocontainers_flask_server/__main__.py
+++ biocontainers-flask/biocontainers_flask_server/__main__.py
@@ -8,7 +8,7 @@ from biocontainers_flask_server import encoder
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'GA4GH Tool Discovery API'})
+    app.add_api('swagger.yaml', arguments={'title': 'BioContainers Discovery API'})
     app.run(port=8080)
 
 
