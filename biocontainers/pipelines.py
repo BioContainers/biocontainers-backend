370a4b126c2c396d5f5691da5c4f702d80960d7e
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-11-26 00:29:13 +0000

    pipelines created

--- biocontainers/pipelines.py
+++ biocontainers/pipelines.py
@@ -1,4 +1,3 @@
-import argparse
 import configparser
 import logging
 
@@ -10,6 +9,13 @@ from biocontainers.github.models import GitHubCondaReader, GitHubConfiguration, 
 from biocontainers.quayio.models import QuayIOReader
 
 logger = logging.getLogger('biocontainers.pipelines')
+
+
+def print_help(ctx, param, value):
+    if value is False:
+        return
+    click.echo(ctx.get_help())
+    ctx.exit()
 
 
 def get_config(file):
@@ -72,19 +78,42 @@ def annotate_docker_recipes(config, config_profile):
     conda_recipes = github_reader.read_docker_recipes()
 
 
+def get_database_uri(param):
+    uri = 'mongodb://' + param['MONGODB_USER'] + ":" + param['MONGODB_PASS'] + '@' + param['MONGODB_HOST'] + ':' + \
+          param['MONGO_PORT'] + '/' + param['BIOCONT_DB_NAME'] + '?ssl=false&authSource=' + param['MONGODB_ADMIN_DB']
+    return uri
+
+
 @click.command()
-@click.option('--import-quayio',     '-q', help='Import Quay.io Recipes', is_flag=True)
-@click.option('--import-docker',     '-k', help="Import Docker Recipes", is_flag=True)
-@click.option('--config-file',       '-c', type=click.Path(), default='configuration.ini')
-@click.option('--database-uri',      '-d', help="Mongo Database URI (e.g. mongodb://localhost:27017/testdb)", envvar='BICONTAINERS_DATABASE_URI')
-@click.option('--database-user',     '-u', help="Mongo database username", envvar='BICONTAINERS_DATABASE_USER')
-@click.option('--database-password', '-p', help="Mongo database password", envvar='BICONTAINERS_DATABASE_PASSWORD')
-@click.option('--config-profile',    '-a', help="This option allow to select a config profile", default='PRODUCTION')
-def main(import_quayio, import_docker, config_file, database_uri, database_user, database_password, config_profile):
+@click.option('--import-quayio', '-q', help='Import Quay.io Recipes', is_flag=True)
+@click.option('--import-docker', '-k', help="Import Docker Recipes", is_flag=True)
+@click.option('--config-file', '-c', type=click.Path(), default='configuration.ini')
+@click.option('--config-profile', '-a', help="This option allow to select a config profile", default='PRODUCTION')
+@click.option('-db', '--db-name', help="Name of the database", envvar='BIOCONT_DB_NAME')
+@click.option('-h', '--db-host', help='Host the database', envvar='MONGODB_HOST')
+@click.option('-auth', '--db-auth-database', help='Authentication database in Mongo', envvar='MONGODB_ADMIN_DB')
+@click.option('-u', '--db-user', help='Database root user', envvar='MONGODB_USER', default='admin')
+@click.option('-pw', '--db-password', help='Database password', envvar='MONGODB_PASS')
+@click.option('-p', '--db-port', help='Database port', envvar='MONGO_PORT', default='27017')
+@click.pass_context
+def main(ctx, import_quayio, import_docker, config_file, config_profile, db_name, db_host, db_auth_database, db_user,
+         db_password, db_port):
+
     config = get_config(config_file)
     if config[config_profile]['VERBOSE'] == "True":
         for key in config[config_profile]:
             print(key + "=" + config[config_profile][key])
+
+    if (db_name is None) or (db_host is None) or (db_user is None):
+        print_help(ctx, None, value=True)
+    else:
+        config[config_profile]['BIOCONT_DB_NAME'] = db_name
+        config[config_profile]['MONGODB_HOST'] = db_host
+        config[config_profile]['MONGO_PORT'] = db_port
+        config[config_profile]['MONGODB_USER'] = db_user
+        config[config_profile]['MONGODB_ADMIN_DB'] = db_auth_database
+        config[config_profile]['MONGODB_PASS'] = db_password
+        config[config_profile]['DATABASE_URI'] = get_database_uri(config[config_profile])
 
     if import_quayio is not False:
         import_quayio_containers(config, config_profile)
