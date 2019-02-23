91c255fc6eb6685ec354924f4448edfa54e5b8d8
chakradhar.bandla <chakrabandla@gmail.com>
2019-02-23 19:05:47 +0900

    minor changes to pipeline's docker run script

--- dockerfiles/pipelines/run.sh
+++ dockerfiles/pipelines/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /biocontainers-backend/biocontainers/pipelines.py --config-file /biocontainers-backend/biocontainers/configuration.ini --db-password $MONGODB_PASS --db-host $MONGODB_HOST --db-auth-database $MONGODB_ADMIN_DB --db-name $BIOCONT_DB_NAME --db-user $MONGODB_USER
+python /biocontainers-backend/biocontainers/pipelines.py --config-file /biocontainers-backend/biocontainers/configuration.ini --db-password $MONGODB_PASS --db-host $MONGODB_HOST --db-auth-database $MONGODB_ADMIN_DB --db-name $BIOCONT_DB_NAME --db-user $MONGODB_USER -k -q
